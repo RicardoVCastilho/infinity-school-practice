32f327fab208ca890e1d1ab60f396a7966e302e9
RicardoVCastilho <seuemail@exemplo.com>
2025-05-14 17:38:58 -0300

    uploading project class 03

---
+++ minicurso-logica-python/index.js
@@ -0,0 +1,7 @@
+let nome = "Ricardo";
+let sobrenome = "Castilho";
+let nomeCompleto = nome + " " + sobrenome;
+let idade = 22;
+let email = "ricardo@gmail.com";
+
+console.log("O meu nome é: " + nomeCompleto + " E eu tenho " + idade + " anos de idade." + "E o meu e-mail é: " + email);
